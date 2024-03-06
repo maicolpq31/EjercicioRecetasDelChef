1f7148b0428c14414c77cc31b94340955d50473d
emmanuelcalad0615 <emmanuelcalad@gmail.com>
2024-03-06 13:37:27 -0500

    segundo commit

--- recetasdelchef/model/chef.py
+++ recetasdelchef/model/chef.py
@@ -24,13 +24,39 @@ class Ingrediente:
         self.unidad: UnidadMedida = unidad
 
 
-    def __str__(self):
-        return f"{self.cantidad} - {self.unidad}> - <{self.alimento}>"          ###??
-
-
-class Receta:
-
+    def __str__(self) -> str:
+        return f"{self.cantidad} - {self.unidad}> - <{self.alimento}>"         ####
 
 # TODO: Implementar la clase Receta
+class Receta:
+    def __init__(self, nombre: str, description: str, etiquetas: None):
+        self.nombre: str = nombre
+        self.ingredientes: list[Ingrediente] = []
+        self.description: str = description
+
+        self.etiquetas: list[str] = [etiquetas]
+        if self.etiquetas == None:         #####
+            lista = []
+
+
+    def agregar_ingrediente(self, alimento: str, cantidad: float, unidad: UnidadMedida):
+        ingrediente = Ingrediente()
+        self.ingredientes.append(ingrediente)
+
+
+    def __str__(self):
+        return f"Receta {self.no}"
 
 # TODO: Implementar la clase Chef
+
+class Chef:
+    def __init__(self):
+        pass
+
+
+    def registrar_receta(self, nombre: str, ingredientes: list(str, float),()):
+        pass
+
+
+    def buscar_receta(self):
+        pass
